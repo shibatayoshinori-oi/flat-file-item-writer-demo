9e6eba0fa0fa38daa976e2a5c288fb71402972d5
Yoshinori Shibata <45994231+shibatayoshinori-oi@users.noreply.github.com>
2023-03-10 21:13:22 +0900

    multiple steps

--- src/main/kotlin/com/example/flatfileitemwriterdemo/BatchConfiguration.kt
+++ src/main/kotlin/com/example/flatfileitemwriterdemo/BatchConfiguration.kt
@@ -3,6 +3,7 @@ package com.example.flatfileitemwriterdemo
 import org.springframework.batch.core.ItemReadListener
 import org.springframework.batch.core.ItemWriteListener
 import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobExecution
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.StepExecution
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer
@@ -12,50 +13,55 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.file.FlatFileItemReader
 import org.springframework.batch.item.file.FlatFileItemWriter
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder
+import org.springframework.batch.item.file.mapping.PassThroughLineMapper
 import org.springframework.batch.item.support.ListItemReader
+import org.springframework.batch.item.support.PassThroughItemProcessor
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.FileSystemResource
-import java.time.LocalDateTime
 
 @Configuration
 @EnableBatchProcessing
 class BatchConfiguration(
     private val jobBuilderFactory: JobBuilderFactory,
     private val stepBuilderFactory: StepBuilderFactory,
-    private val itemWriter: FlatFileItemWriter<String>
+    private val itemWriter1: FlatFileItemWriter<String>,
+    private val itemWriter2: FlatFileItemWriter<String>
 ) : DefaultBatchConfigurer() {
 
     @Bean
     fun job(): Job {
         return jobBuilderFactory.get("sampleJob")
-            .start(step())
+            .start(step1())
+            .next(step2())
             .build()
     }
 
     @Bean
-    fun step(): Step {
-        return stepBuilderFactory.get("sampleStep")
+    fun step1(): Step {
+        return stepBuilderFactory.get("sampleStep1")
             .chunk<Int, String>(1_000)
-            .reader(itemReader())
-            .processor(itemProcessor())
-            .writer(itemWriter)
-            .listener(itemReadListener())
-            .listener(itemWriteListener())
+            .reader(itemReader1())
+            .processor(itemProcessor1())
+            .writer(itemWriter1)
+            .listener(itemReadListener1())
+            .listener(itemWriteListener1())
             .build()
     }
 
     @StepScope
     @Bean
-    fun itemReader(): ItemReader<Int> {
+    fun itemReader1(): ItemReader<Int> {
         return ListItemReader(IntRange(1, 3).toList())
     }
 
     @Bean
-    fun itemReadListener(): ItemReadListener<Int> {
+    fun itemReadListener1(): ItemReadListener<Int> {
         return object : ItemReadListener<Int> {
             override fun beforeRead() {
                 //NOOP
@@ -73,7 +79,7 @@ class BatchConfiguration(
 
     @StepScope
     @Bean
-    fun itemProcessor(): ItemProcessor<Int, String> {
+    fun itemProcessor1(): ItemProcessor<Int, String> {
         return ItemProcessor {
             println("process $it")
             it.toString()
@@ -82,26 +88,18 @@ class BatchConfiguration(
 
     @StepScope
     @Bean
-    fun itemWriter(
+    fun itemWriter1(
         @Value("#{stepExecution}") stepExecution: StepExecution
     ): FlatFileItemWriter<String> {
         return FlatFileItemWriterBuilder<String>()
-            .name("sampleWriter")
-            .resource(FileSystemResource("sample.csv.${LocalDateTime.now()}.csv"))
-            .headerCallback {
-                println("write header with stepExecution:$stepExecution")
-                it.write("count:${stepExecution.readCount}")
-            }
-            .footerCallback {
-                println("write footer with stepExecution:$stepExecution")
-                it.write("count:${stepExecution.readCount}")
-            }
+            .name("sampleWriter1")
+            .resource(FileSystemResource("sample.csv.data-only"))
             .lineAggregator { it }
             .build()
     }
 
     @Bean
-    fun itemWriteListener(): ItemWriteListener<String> {
+    fun itemWriteListener1(): ItemWriteListener<String> {
         return object : ItemWriteListener<String> {
             override fun beforeWrite(items: MutableList<out String>) {
                 //NOOP
@@ -115,5 +113,44 @@ class BatchConfiguration(
                 //NOOP
             }
         }
+    }
+
+    @Bean
+    fun step2(): Step {
+        return stepBuilderFactory.get("sampleStep2")
+            .chunk<String, String>(1_000)
+            .reader(itemReader2())
+            .processor(PassThroughItemProcessor())
+            .writer(itemWriter2)
+            .build()
+    }
+
+    @StepScope
+    @Bean
+    fun itemReader2(): FlatFileItemReader<String> {
+        return FlatFileItemReaderBuilder<String>()
+            .name("sampleReader2")
+            .resource(FileSystemResource("sample.csv.data-only"))
+            .lineMapper(PassThroughLineMapper())
+            .build()
+    }
+
+    @StepScope
+    @Bean
+    fun itemWriter2(
+        @Value("#{stepExecution.jobExecution}") jobExecution: JobExecution,
+    ): FlatFileItemWriter<String> {
+        return FlatFileItemWriterBuilder<String>()
+            .name("sampleWriter2")
+            .resource(FileSystemResource("sample.csv"))
+            .headerCallback { writer ->
+                val step1 = jobExecution.stepExecutions.find { it.stepName == "sampleStep1" }
+                writer.write("this is a header. count:${step1!!.readCount}")
+            }
+            .footerCallback {
+                it.write("this is a footer.")
+            }
+            .lineAggregator { it }
+            .build()
     }
 }
