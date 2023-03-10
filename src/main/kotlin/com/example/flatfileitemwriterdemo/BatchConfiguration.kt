c2b40c24a06de78fe6945a527de09cb7380e2989
Yoshinori Shibata <45994231+shibatayoshinori-oi@users.noreply.github.com>
2023-03-10 20:29:30 +0900

    Check header writing

--- src/main/kotlin/com/example/flatfileitemwriterdemo/BatchConfiguration.kt
+++ src/main/kotlin/com/example/flatfileitemwriterdemo/BatchConfiguration.kt
@@ -1,7 +1,10 @@
 package com.example.flatfileitemwriterdemo
 
+import org.springframework.batch.core.ItemReadListener
+import org.springframework.batch.core.ItemWriteListener
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.StepExecution
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
@@ -9,18 +12,21 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.batch.item.ItemReader
-import org.springframework.batch.item.ItemWriter
+import org.springframework.batch.item.file.FlatFileItemWriter
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder
 import org.springframework.batch.item.support.ListItemReader
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.FileSystemResource
+import java.time.LocalDateTime
 
 @Configuration
 @EnableBatchProcessing
 class BatchConfiguration(
     private val jobBuilderFactory: JobBuilderFactory,
-    private val stepBuilderFactory: StepBuilderFactory
+    private val stepBuilderFactory: StepBuilderFactory,
+    private val itemWriter: FlatFileItemWriter<String>
 ) : DefaultBatchConfigurer() {
 
     @Bean
@@ -36,31 +42,78 @@ class BatchConfiguration(
             .chunk<Int, String>(1_000)
             .reader(itemReader())
             .processor(itemProcessor())
-            .writer(itemWriter())
+            .writer(itemWriter)
+            .listener(itemReadListener())
+            .listener(itemWriteListener())
             .build()
     }
 
     @StepScope
     @Bean
     fun itemReader(): ItemReader<Int> {
-        return ListItemReader(IntRange(1, 5).toList())
+        return ListItemReader(IntRange(1, 3).toList())
+    }
+
+    @Bean
+    fun itemReadListener(): ItemReadListener<Int> {
+        return object : ItemReadListener<Int> {
+            override fun beforeRead() {
+                //NOOP
+            }
+
+            override fun afterRead(item: Int) {
+                println("read $item")
+            }
+
+            override fun onReadError(ex: Exception) {
+                //NOOP
+            }
+        }
     }
 
     @StepScope
     @Bean
     fun itemProcessor(): ItemProcessor<Int, String> {
-        return ItemProcessor { it.toString() }
+        return ItemProcessor {
+            println("process $it")
+            it.toString()
+        }
     }
 
     @StepScope
     @Bean
-    fun itemWriter(): ItemWriter<String> {
+    fun itemWriter(
+        @Value("#{stepExecution}") stepExecution: StepExecution
+    ): FlatFileItemWriter<String> {
         return FlatFileItemWriterBuilder<String>()
             .name("sampleWriter")
-            .resource(FileSystemResource("sample.csv"))
-            .headerCallback { it.write("header") }
-            .footerCallback { it.write("footer") }
+            .resource(FileSystemResource("sample.csv.${LocalDateTime.now()}.csv"))
+            .headerCallback {
+                println("write header with stepExecution:$stepExecution")
+                it.write("count:${stepExecution.readCount}")
+            }
+            .footerCallback {
+                println("write footer with stepExecution:$stepExecution")
+                it.write("count:${stepExecution.readCount}")
+            }
             .lineAggregator { it }
             .build()
+    }
+
+    @Bean
+    fun itemWriteListener(): ItemWriteListener<String> {
+        return object : ItemWriteListener<String> {
+            override fun beforeWrite(items: MutableList<out String>) {
+                //NOOP
+            }
+
+            override fun afterWrite(items: MutableList<out String>) {
+                println("wrote $items")
+            }
+
+            override fun onWriteError(ex: Exception, items: MutableList<out String>) {
+                //NOOP
+            }
+        }
     }
 }
